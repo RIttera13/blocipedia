21bd02bea3749a1013d591641b1800a6b52f0ae7
Drew <rittera13@gmail.com>
2016-12-05 11:34:38 -0600

    blocipedia-checkpoint6, created seed file using faker. Added seeded user for each role for me to access for testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do
+  User.create!(
+  email:    Faker::Internet.email,
+  password: Faker::Internet.password
+  )
+end
+
+users = User.all
+
+20.times do
+  Wiki.create!(
+    title: Faker::StarWars.character,
+    body: Faker::ChuckNorris.fact
+  )
+end
+topics = Wiki.all
+
+admin = User.create!(
+  email:    'admin@b.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+premium = User.create!(
+  email:    'premium@b.com',
+  password: 'helloworld',
+  role:     'premium'
+)
+
+standard = User.create!(
+  email:    'standard@b.com',
+  password: 'helloworld'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
